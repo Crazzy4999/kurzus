24ced250ad4f966c7ee6d5823f9c08ed5c1b375a
Fábián Álmos <fabian.almos77@gmail.com>
2023-05-19 16:35:34 +0200

    Moved logic to file directory

---
+++ httpAssignment/db/repo/file/suppliers.go
@@ -0,0 +1,34 @@
+package repo
+
+import (
+	"encoding/json"
+	"fmt"
+	"httpA/db/model"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+type SupplierRepository struct{}
+
+func (s *SupplierRepository) GetAll() ([]model.Supplier, error) {
+	suppliers := make([]model.Supplier, 0)
+	info, err := ioutil.ReadDir("data/suppliers")
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, file := range info {
+		supplier := new(model.Supplier)
+		content, err := os.Open(fmt.Sprintf("data/suppliers/%s", file.Name()))
+		if err != nil {
+			fmt.Println(err)
+		}
+		bytes, err := io.ReadAll(content)
+		_ = json.Unmarshal(bytes, supplier)
+		if err != nil {
+			fmt.Println(err)
+		}
+		suppliers = append(suppliers, *supplier)
+	}
+	return suppliers, err
+}
