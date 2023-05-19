4a03ea1e7aa5a146cd50bbf81aabd3c0386398a9
Fábián Álmos <fabian.almos77@gmail.com>
2023-05-19 14:56:49 +0200

    Added basic Supplier struct

---
+++ httpAssignment/db/model/supplier.go
@@ -0,0 +1,18 @@
+package main
+
+type SupplierType string
+
+const (
+	RESTAURANT  SupplierType = "restaurant"
+	COFFEE_SHOP SupplierType = "coffee_shop"
+	SUPERMARKET SupplierType = "supermarket"
+)
+
+type Supplier struct {
+	Id      int          `json:"id"`
+	Name    string       `json:"name"`
+	Type    SupplierType `json:"type"`
+	Image   string       `json:"image"`
+	Opening string       `json:"opening"`
+	Closing string       `json:"closing"`
+}
