05d4b60a701effa61136d456010ce47a5bf3ac6b
Fábián Álmos <fabian.almos77@gmail.com>
2023-06-01 22:08:19 +0200

    Added Route struct

--- httpAssignment/endpoint/router/router.go
+++ httpAssignment/endpoint/router/router.go
@@ -1,10 +1,31 @@
 package router
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type Route struct {
+	method  string
+	url     string
+	handler http.Handler
+}
 
 type Router struct {
-	Port   string
+	port   string
 	Params []parameter
+	Routes []Route
+}
+
+func NewRouter(port string) *Router {
+	router := new(Router)
+	router.port = port
+	return router
+}
+
+func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// if serve http is implemented than we can serve pages based on a result
+	http.NotFound(w, r)
 }
 
 type parameter string
@@ -34,25 +55,45 @@ func (router *Router) parseURL(url string) []parameter {
 	return params
 }
 
-func (router *Router) POST(url string) {
-
-}
-
-func (router *Router) GET(url string) {
-	router.parseURL(url)
-	for i, p := range router.Params {
-		fmt.Println(i, p)
+func (router *Router) GetParam(param parameter) parameter {
+	for _, p := range router.Params {
+		if p == param {
+			return p
+		}
 	}
+	return ""
 }
 
-func (router *Router) PUT(url string) {
+func (router *Router) addRoute() {
 
 }
 
-func (router *Router) DELETE(url string) {
+func (router *Router) POST(url string, f func(w http.ResponseWriter, r *http.Request)) {
+
+}
+
+func (router *Router) GET(url string, f func(w http.ResponseWriter, r *http.Request)) {
+	router.parseURL(url)
+
+	/*
+		When there is a dynamic parameter like :id
+		then parse the url to 'suppliers/' and a map of parameters with any values like
+		map[":id"] = 1
+		than based on the new url that's going to be generated which is suppliers/1
+		run the ServeHTTP function on that handler which is passed by 'f' of type func(...) {}
+	*/
+
+}
+
+func (router *Router) PUT(url string, f func(w http.ResponseWriter, r *http.Request)) {
+
+}
+
+func (router *Router) DELETE(url string, f func(w http.ResponseWriter, r *http.Request)) {
 
 }
 
 func (router *Router) Start() {
-	//router.Port
+	fmt.Printf("Server started on: localhost%s\n", router.port)
+	http.ListenAndServe(router.port, router)
 }
