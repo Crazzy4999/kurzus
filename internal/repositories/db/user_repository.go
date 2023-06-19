2949f045eb870feb55bee1a5f8ce8cf298ed3971
Fábián Álmos <fabian.almos77@gmail.com>
2023-06-19 22:46:10 +0200

    Setup basic UserRepository

--- internal/repositories/db/user_repository.go
+++ internal/repositories/db/user_repository.go
@@ -1,7 +1,31 @@
 package db
 
+import (
+	"hangryAPI/internal/models"
+)
+
 type UserRepository struct{}
 
 func NewUserRepository() *UserRepository {
 	return &UserRepository{}
+}
+
+func (ur *UserRepository) Create(u *models.User) error {
+	return nil
+}
+
+func (ur *UserRepository) GetAll() ([]*models.User, error) {
+	return nil, nil
+}
+
+func (ur *UserRepository) GetUserByID(id int) (*models.User, error) {
+	return nil, nil
+}
+
+func (ur *UserRepository) Update(u *models.User) error {
+	return nil
+}
+
+func (ur *UserRepository) Delete(id int) error {
+	return nil
 }
