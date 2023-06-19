97427840bd6de9686bd724250114ad61a3ad2923
Fábián Álmos <fabian.almos77@gmail.com>
2023-06-19 19:06:43 +0200

    Setup routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from "@/views/HomeView.vue"
+import LoginView from "@/views/LoginView.vue"
+import SignUpView from "@/views/SignUpView.vue"
+import ProfileView from "@/views/ProfileView.vue"
 import SuppliersView from "@/views/SuppliersView.vue"
+import CategoriesView from "@/views/CategoriesView.vue"
+import SupplierView from "@/views/SupplierView.vue"
+import HistoryView from "@/views/HistoryView.vue"
+import CartView from "@/views/CartView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,21 +17,35 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
       meta: {
-        title: 'Hangry - Home page',
-        metaTags: [
-          {
-            name: 'description',
-            content: 'The home page of our example app.'
-          },
-          {
-            property: 'og:description',
-            content: 'The home page of our example app.'
-          }
-        ]
+        title: 'Hangry - Home page'
       }
     },
     {
-      path: '/',
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+      meta: {
+        title: 'Hangry - Login'
+      }
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUpView,
+      meta: {
+        title: 'Hangry - Sign up'
+      }
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView,
+      meta: {
+        title: 'Hangry - Profile'
+      }
+    },
+    {
+      path: '/suppliers',
       name: 'suppliers',
       component: SuppliersView,
       meta: {
@@ -32,11 +53,35 @@ const router = createRouter({
       }
     },
     {
-      path: '/',
-      name: 'suppliers',
-      component: SuppliersView,
+      path: '/categories',
+      name: 'categories',
+      component: CategoriesView,
       meta: {
-        title: 'Hangry - Suppliers'
+        title: 'Hangry - Categories'
+      }
+    },
+    {
+      path: '/supplier',
+      name: 'supplier',
+      component: SupplierView,
+      meta: {
+        title: 'Hangry' //might need some other approach if possible
+      }
+    },
+    {
+      path: '/history',
+      name: 'history',
+      component: HistoryView,
+      meta: {
+        title: 'Hangry - History'
+      }
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: CartView,
+      meta: {
+        title: 'Hangry - Cart'
       }
     }
   ]
