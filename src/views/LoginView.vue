cd6a00bdf76689f3cad3c98e757fa17b64b5386c
Fábián Álmos <fabian.almos77@gmail.com>
2023-06-22 18:46:12 +0200

    Added AuthForm component, Login and Signup pages

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import AuthHeader from '@/components/AuthHeader.vue';
+import AuthForm, { type inputVal } from '@/components/form/AuthForm.vue';
 
+let inputs: inputVal[] = [
+    { name: "email", type: "email", placeholder: "email", required: true },
+    { name: "password", type: "password", placeholder: "password", required: true }
+]
 </script>
 
 <template>
-
+    <AuthHeader/>
+    <AuthForm :inputs="inputs" infoText="Don't have an account yet?" btnText="Login" alternateText="Sign up now!" alternateUrl="/signup"/>
 </template>
 
 <style scoped>
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import AuthHeader from '@/components/AuthHeader.vue';
+import AuthForm, { type inputVal } from '@/components/form/AuthForm.vue';
 
+let inputs: inputVal[] = [
+    { name: "firstName", type: "text", placeholder: "first name", required: true },
+    { name: "lastName", type: "text", placeholder: "last name", required: true },
+    { name: "email", type: "email", placeholder: "email", required: true },
+    { name: "password", type: "password", placeholder: "password", required: true },
+    { name: "passwordAgain", type: "password", placeholder: "password again", required: true }
+]
 </script>
 
 <template>
-
+    <AuthHeader/>
+    <AuthForm :inputs="inputs" infoText="Already have an account?" btnText="Sign Up" alternateText="Log in!" alternateUrl="/login"/>
 </template>
 
 <style scoped>
