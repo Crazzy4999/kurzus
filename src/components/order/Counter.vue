57249fcc05445e87939e9d1615d0240d796cd633
Fábián Álmos <fabian.almos77@gmail.com>
2023-06-24 13:28:11 +0200

    Added counter for cart page

---
+++ src/components/order/Counter.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+let count = ref(0)
+
+function decrement() {
+    if(0 <= count.value - 1) {
+        count.value--
+    }
+}
+
+function restrictInput(e: KeyboardEvent) {
+    if(!/\d/.test(e.key)) return e.preventDefault()
+}
+</script>
+
+<template>
+    <div class="counter-container">
+        <button class="decrement common" @click="decrement">-</button>
+        <span class="count-wrapper common">
+            <input class="count" type="text" :value="count" @keypress="(e) => restrictInput(e as KeyboardEvent)">
+        </span>
+        <button class="increment common" @click="count++">+</button>
+    </div>
+</template>
+
+<style scoped>
+.counter-container {
+    position: relative;
+    display: flex;
+}
+
+.common {
+    font-size: var(--p-size);
+    font-weight: 750;
+    color: var(--second-color);
+    border: calc(var(--sub-border-size) * 1.25) solid var(--settings-color)
+}
+
+button {
+    position: relative;
+    cursor: pointer;
+    margin-block: auto;
+}
+
+.decrement {
+    border-right: none;
+    border-top-left-radius: var(--sub-p-size);
+    border-bottom-left-radius: var(--sub-p-size);
+    padding-inline: var(--sub-p-size) var(--tiny-size);
+}
+
+.count-wrapper {
+    display: grid;
+    width: var(--h3-size);
+}
+
+.count {
+    text-align: center;
+    font-size: var(--p-size);
+    font-weight: 750;
+    color: var(--second-color);
+    max-width: var(--h3-size);
+    margin: auto;
+}
+
+.increment {
+    border-left: none;
+    border-top-right-radius: var(--sub-p-size);
+    border-bottom-right-radius: var(--sub-p-size);
+    padding-inline: var(--tiny-size) var(--sub-p-size);
+}
+</style>
