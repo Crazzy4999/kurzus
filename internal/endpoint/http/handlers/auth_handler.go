902a8ad2698304fc28c6a7e9338dcd3d3c6c3e62
Fábián Álmos <fabian.almos77@gmail.com>
2023-06-28 16:27:01 +0200

    Added bases of authentication

--- internal/endpoint/http/handlers/auth_handler.go
+++ internal/endpoint/http/handlers/auth_handler.go
@@ -1,31 +1,147 @@
 package handler
 
 import (
+	"encoding/json"
 	config "hangryAPI/configs"
-	repository "hangryAPI/internal/repositories"
+	request "hangryAPI/internal/endpoint/http/requests"
+	"hangryAPI/internal/endpoint/http/responses"
+	"hangryAPI/internal/models"
+	"hangryAPI/internal/repositories/db"
+	"hangryAPI/internal/service/token"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthHandler struct {
-	userRepo *repository.UserRepositoryI
+	userRepo *db.UserRepository
 	cfg      *config.Config
 }
 
-func NewAuthHandler(ur repository.UserRepositoryI, cfg *config.Config) *AuthHandler {
+func NewAuthHandler(ur *db.UserRepository, cfg *config.Config) *AuthHandler {
 	return &AuthHandler{
-		userRepo: &ur,
+		userRepo: ur,
 		cfg:      cfg,
 	}
 }
 
 func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
+	req := new(request.LoginRequest)
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
+	user, err := h.userRepo.GetUserByEmail(req.Email)
+	if err != nil {
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	tokenService := token.NewTokenService(h.cfg)
+
+	accessString, err := tokenService.GenerateAccessToken(user.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	refreshString, err := tokenService.GenerateRefreshToken(user.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	resp := responses.TokenPairResponse{
+		AccessToken:  accessString,
+		RefreshToken: refreshString,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(resp)
 }
 
 func (h *AuthHandler) SignUp(w http.ResponseWriter, r *http.Request) {
+	req := new(request.SignUpRequest)
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
+	if req.Password != req.PasswordAgain {
+		http.Error(w, "Passwords doesn't match!", http.StatusBadRequest)
+		return
+	}
+
+	users, err := h.userRepo.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for _, user := range users {
+		if user.Email == req.Email {
+			http.Error(w, "Email already in use!", http.StatusBadRequest)
+			return
+		}
+	}
+
+	password, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, "Couldn't generate password!", http.StatusBadRequest)
+		return
+	}
+
+	user := &models.User{
+		Name:     req.Name,
+		Email:    req.Email,
+		Password: string(password),
+	}
+
+	h.userRepo.Create(user)
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func (h *AuthHandler) Refresh(w http.ResponseWriter, r *http.Request) {
+	tokenService := token.NewTokenService(h.cfg)
 
+	claims, err := tokenService.GetClaims(r, h.cfg.RefreshSecret)
+	if err != nil {
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	accessString, err := tokenService.GenerateAccessToken(claims.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	refreshString, err := tokenService.GenerateRefreshToken(claims.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	resp := responses.TokenPairResponse{
+		AccessToken:  accessString,
+		RefreshToken: refreshString,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(resp)
+}
+
+func (h *AuthHandler) ResetPassword(w http.ResponseWriter, r *http.Request) {
+	req := new(request.ResetPasswordRequest)
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 }
