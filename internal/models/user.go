f58e439a7544c2ab256002e31f2afa57da340deb
Fábián Álmos <fabian.almos77@gmail.com>
2023-06-28 20:53:13 +0200

    Implemented functions of user repo

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,10 +1,12 @@
 package models
 
+import "database/sql"
+
 type User struct {
-	ID        int    `json:"id"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
-	Address   string `json:"address"`
+	ID        int            `json:"id"`
+	Address   sql.NullString `json:"address"`
+	FirstName string         `json:"firstName"`
+	LastName  string         `json:"lastName"`
+	Email     string         `json:"email"`
+	Password  string         `json:"password"`
 }
--- internal/repositories/db/user_repository.go
+++ internal/repositories/db/user_repository.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 	"hangryAPI/internal/models"
 )
 
@@ -17,13 +18,19 @@ func NewUserRepository(db *sql.DB) *UserRepository {
 }
 
 func (ur *UserRepository) Create(u *models.User) error {
-	stmt, err := ur.db.Prepare("INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4);")
+	stmt, err := ur.db.Prepare("INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)")
 	if err != nil {
 		return errors.New("couldn't prepare statment to insert new user")
 	}
 
+	user, _ := ur.GetUserByEmail(u.Email)
+	if user != nil {
+		return errors.New("user already exist with this email")
+	}
+
 	_, err = stmt.Exec(u.FirstName, u.LastName, u.Email, u.Password)
 	if err != nil {
+		fmt.Println(err)
 		return errors.New("insert into users failed")
 	}
 
@@ -31,17 +38,73 @@ func (ur *UserRepository) Create(u *models.User) error {
 }
 
 func (ur *UserRepository) GetAll() ([]*models.User, error) {
-	return nil, nil
+	stmt, err := ur.db.Prepare("SELECT id, address_id, first_name, last_name, email, password FROM users")
+	if err != nil {
+		return nil, errors.New("couldn't prepare statement to get all users")
+	}
+
+	rows, err := stmt.Query()
+	if err != nil {
+		return nil, errors.New("getting all users failed")
+	}
+	defer rows.Close()
+
+	var users []*models.User
+	for rows.Next() {
+		user := &models.User{}
+		err = rows.Scan(&user.ID, &user.Address, &user.FirstName, &user.LastName, &user.Email, &user.Password)
+		if err == sql.ErrNoRows {
+			return nil, nil
+		} else if err != nil {
+			return nil, errors.New("types mismatch during the scanning")
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
 }
 
 func (ur *UserRepository) GetUserByEmail(email string) (*models.User, error) {
-	return nil, nil
+	stmt, err := ur.db.Prepare("SELECT id, address_id, first_name, last_name, email, password FROM users WHERE users.email = $1")
+	if err != nil {
+		return nil, errors.New("couldn't prepare statement to get user by email")
+	}
+
+	var user models.User
+
+	row := stmt.QueryRow(email)
+	err = row.Scan(&user.ID, &user.Address, &user.FirstName, &user.LastName, &user.Email, &user.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func (ur *UserRepository) Update(u *models.User) error {
+	stmt, err := ur.db.Prepare("UPDATE users SET address_id = $2, first_name = $1, last_name = $3, password = $4 WHERE users.id = $5")
+	if err != nil {
+		return errors.New("couldn't prepare statement for updating user")
+	}
+
+	_, err = stmt.Exec(u.Address, u.FirstName, u.LastName, u.Password, u.ID)
+	if err != nil {
+		return errors.New("updating user failed")
+	}
+
 	return nil
 }
 
 func (ur *UserRepository) Delete(id int) error {
+	stmt, err := ur.db.Prepare("DELETE FROM users WHERE users.id = $1")
+	if err != nil {
+		return errors.New("couldn't prepare statement for deleting user by id")
+	}
+
+	_, err = stmt.Exec(id)
+	if err != nil {
+		return errors.New("deleting user by id failed")
+	}
+
 	return nil
 }
