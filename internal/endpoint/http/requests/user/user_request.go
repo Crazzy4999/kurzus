696430e658158ee9d4b126f9875723fecfb77016
Fábián Álmos <fabian.almos77@gmail.com>
2023-07-06 10:55:06 +0200

    Changed address data type to int from int64

--- internal/endpoint/http/requests/user/user_request.go
+++ internal/endpoint/http/requests/user/user_request.go
@@ -1,7 +1,7 @@
 package request
 
 type UserRequest struct {
-	Address   int64  `json:"address"`
+	Address   int    `json:"address"`
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
 }
