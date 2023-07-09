c42706f97d8f274868e1cc2d408b84ce7a9a8629
Fábián Álmos <fabian.almos77@gmail.com>
2023-07-09 10:58:38 +0200

    Added new middleware for refresh token and access token

--- internal/endpoint/middleware/middleware.go
+++ internal/endpoint/middleware/middleware.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"context"
 	config "hangryAPI/configs"
+	handler "hangryAPI/internal/endpoint/http/handlers"
 	"hangryAPI/internal/service/token"
 	"hangryAPI/internal/util"
 	"net/http"
@@ -10,14 +11,14 @@ import (
 
 type Func func(handler http.Handler) http.Handler
 
-func CheckTokenValidity(next http.Handler) http.Handler {
+func CheckAccessTokenValidity(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg := config.NewConfig(false)
 		tokenService := token.NewTokenService(cfg)
 
-		claims, err := tokenService.GetClaims(r, cfg.RefreshSecret)
+		claims, err := tokenService.GetClaims(r, cfg.AccessSecret)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusUnauthorized)
+			http.Error(w, handler.INVALID_CREDENTIALS, http.StatusUnauthorized)
 			return
 		}
 
@@ -26,6 +27,24 @@ func CheckTokenValidity(next http.Handler) http.Handler {
 
 		if claims != nil {
 			next.ServeHTTP(w, rctx)
+			return
+		}
+	})
+}
+
+func CheckRefreshTokenValidity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg := config.NewConfig(false)
+		tokenService := token.NewTokenService(cfg)
+
+		claims, err := tokenService.GetClaims(r, cfg.RefreshSecret)
+		if err != nil {
+			http.Error(w, handler.INVALID_CREDENTIALS, http.StatusUnauthorized)
+			return
+		}
+
+		if claims != nil {
+			next.ServeHTTP(w, r)
 			return
 		}
 	})
