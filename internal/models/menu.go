5fbb290504ec28657a1a58919f3696a256e0917f
Fábián Álmos <fabian.almos77@gmail.com>
2023-07-11 23:53:51 +0200

    Changed fields order in menu model

--- internal/models/menu.go
+++ internal/models/menu.go
@@ -3,8 +3,8 @@ package models
 type Menu struct {
 	ID         int     `json:"id"`
 	Name       string  `json:"name"`
+	Image      string  `json:"image"`
 	SupplierID int     `json:"supplierID"`
 	CategoryID int     `json:"categoryID"`
-	Image      string  `json:"image"`
 	Price      float32 `json:"price"`
 }
